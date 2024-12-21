76b4080f73a168a291d25f445e24407c41a71175
DRAVEN0P <d.neogi.coding@gmail.com>
2024-12-21 19:24:09 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Daipayan Neogi on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
